0f9afbe476183e9a4f81955452fa6c4f2f81c39e
ManhTaif <manhtai614@gmail.com>
2025-05-21 22:48:13 +0700

    first cmomit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NgoManhTai_2121051382_KiemTra_2425.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
